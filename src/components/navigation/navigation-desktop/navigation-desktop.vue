34a86c4a9a12e76bbf86e6d1ad953c0194be0487
luketevers <luketevers@gmail.com>
2019-04-02 10:29:41 -0700

    feat(navigation-desktop): refactors all functions and builds out the dropdown menu;

--- src/components/navigation/navigation-desktop/navigation-desktop.vue
+++ src/components/navigation/navigation-desktop/navigation-desktop.vue
@@ -1,10 +1,66 @@
-<template lang="pug" src="./navigation-desktop.pug"></template>
+<template lang='pug' src='./navigation-desktop.pug'></template>
 
 <script>
+
 export default {
   computed: {
+    content () {
+      return this.$store.state.app.navigation
+    },
     links () {
-      return this.$router.options.routes
+      return this.$router.options.routes.filter(item => item.navigation)
+    },
+    navScrolled () {
+      return this.offsetTop >= (window.innerHeight * 0.45)
+    }
+  },
+  data () {
+    return {
+      dropdownShow: false,
+      dropdownSmooth: false,
+      height: 0,
+      left: 0,
+      loaded: false,
+      offsetTop: null,
+      showing: null
+    }
+  },
+  created () {
+    window.addEventListener('scroll', () => {
+      this.offsetTop = window.pageYOffset
+    })
+  },
+  watch: {
+    'content' (data) {
+      if (typeof data !== 'undefined') {
+        this.loaded = true
+      }
+    }
+  },
+  methods: {
+    pageLink (i) {
+      this.$router.push(i.path)
+    },
+    hoverDropdown (i, k) {
+      let left = this.$refs.root[i].offsetLeft
+      let width = this.$refs.root[i].clientWidth
+      this.showing = i
+      this.left = left + width / 2 + 'px'
+      this.$nextTick(this.setHeight(i))
+    },
+    setHeight (i) {
+      this.height = this.$refs.link[i].clientHeight + 16 + 'px'
+    },
+    dropdownActive () {
+      this.dropdownShow = true
+      setTimeout(() => {
+        this.dropdownSmooth = true
+      }, 500)
+    },
+    dropdownLeave () {
+      // this.dropdownShow = false
+      // this.dropdownSmooth = false
+      // this.showing = null
     }
   }
 }
