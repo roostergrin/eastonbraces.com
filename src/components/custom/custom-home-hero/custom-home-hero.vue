bbbb4d9a6d837a910bc81606f8b41e29a8d4440e
luketevers <luketevers@gmail.com>
2019-04-03 14:36:31 -0700

    creates(custom-home-hero): creates on desktop;

---
+++ src/components/custom/custom-home-hero/custom-home-hero.vue
@@ -0,0 +1,34 @@
+<template lang='pug' src='./custom-home-hero.pug'></template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      activeSlide: 0
+    }
+  },
+  mounted () {
+    this.setActive()
+  },
+  methods: {
+    setActive () {
+      setInterval(() => {
+        this.activeSlide > (this.content.length - 2) ? this.activeSlide = 0 : this.activeSlide += 1
+      }, 7500)
+    },
+    setNext () {
+      this.activeSlide !== (this.content.length - 1) ? this.activeSlide += 1 : this.activeSlide = 0
+      console.log('clicked', this.activeSlide)
+    },
+    setPrev () {
+      this.activeSlide !== 0 ? this.activeSlide -= 1 : this.activeSlide = this.content.length - 1
+      console.log('clicked', this.activeSlide)
+    }
+  }
+}
+</script>
