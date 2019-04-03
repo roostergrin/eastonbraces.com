0c61786e1e1746d5d0b8df48e56f1b48d6da322a
luketevers <luketevers@gmail.com>
2019-04-03 14:34:31 -0700

    creates(block-facebook): creates component on desktop;

---
+++ src/components/block/block-facebook/block-facebook.vue
@@ -0,0 +1,26 @@
+<template lang='pug' src='./block-facebook.pug'></template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      activeImage: 0
+    }
+  },
+  mounted () {
+    this.switchImage()
+  },
+  methods: {
+    switchImage () {
+      setInterval(() => {
+        this.activeImage < (this.content.images.length - 1) ? this.activeImage += 1 : this.activeImage = 0
+      }, 5000)
+    }
+  }
+}
+</script>
