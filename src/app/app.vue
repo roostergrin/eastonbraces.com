ada27cfeba775bf2badc0ee06a60c229c8d47377
luketevers <luketevers@gmail.com>
2019-04-04 15:02:56 -0700

    creates(the-footer): creates on desktop;

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,7 +1,8 @@
 <template lang="pug" src="./app.pug"></template>
 
 <script>
-import NavigationDesktop from 'components/navigation/navigation-desktop/navigation-desktop'
+import NavigationDesktop from 'components/the-navigation/navigation-desktop/navigation-desktop'
+import TheFooter from 'components/the-footer/the-footer'
 
 export default {
   async created () {
@@ -14,7 +15,8 @@ export default {
     this.$store.dispatch('VIEW_MODALCONTENT', false)
   },
   components: {
-    NavigationDesktop
+    NavigationDesktop,
+    TheFooter
   }
 }
 </script>
---
+++ src/components/the-footer/the-footer.vue
@@ -0,0 +1,17 @@
+<template lang='pug' src='./the-footer.pug'></template>
+
+<script>
+export default {
+  computed: {
+    links () {
+      return this.$router.options.routes.filter(link => link.list)
+    },
+    content () {
+      return this.$store.state.app['the-footer']
+    }
+  },
+  mounted () {
+    console.log(this.links)
+  }
+}
+</script>
