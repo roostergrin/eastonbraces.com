7d7cf1fd0edf8c5aa683e786af95a7fc34cb63f6
luketevers <luketevers@gmail.com>
2019-04-05 14:50:36 -0700

    updates(base-hero): completes on desktop;

--- src/components/base/base-hero/base-hero.vue
+++ src/components/base/base-hero/base-hero.vue
@@ -1,10 +1,35 @@
-<template lang='pug' src='./hero.pug'></template>
+<template lang='pug' src='./base-hero.pug'></template>
 
 <script>
 export default {
   props: {
     content: {
       type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      activeLink: null
+    }
+  },
+  watch: {
+    '$route' (data) {
+      let anchor = data.hash.replace(/#(\S)/g, '$1')
+      this.activeLink = anchor
+    }
+  },
+  mounted () {
+    if (this.$route.hash) {
+      let anchor = this.$route.hash.replace(/#(\S)/g, '$1')
+      this.activeLink = anchor
+    } else {
+      this.activeLink = this.$route.meta[0].anchor
+    }
+  },
+  methods: {
+    changeSection (i) {
+      this.$router.push(i.path)
+      this.activeLink = i.anchor
     }
   }
 }
