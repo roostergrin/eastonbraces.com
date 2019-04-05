f09cb597118174a1fffdcfaab9515192aa1a3cea
luketevers <luketevers@gmail.com>
2019-04-05 14:49:55 -0700

    updates(about-us): adds subsection components for each page;

--- src/pages/about-us/about-us.vue
+++ src/pages/about-us/about-us.vue
@@ -1,12 +1,17 @@
 <template lang="pug" src="./about-us.pug"></template>
 
 <script>
+import CustomAboutDoctor from 'components/custom/custom-about-doctor/custom-about-doctor'
+
 export default {
   name: 'about-us',
   computed: {
     props () {
       return this.$store.state.pages['about-us']
     }
+  },
+  components: {
+    CustomAboutDoctor
   }
 }
 </script>
