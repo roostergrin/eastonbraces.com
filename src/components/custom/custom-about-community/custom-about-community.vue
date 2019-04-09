26332a591da9b0dba75fc49ad5d18a579994088d
luketevers <luketevers@gmail.com>
2019-04-09 10:09:15 -0700

    creates(custom-about-community): creates on desktop;

---
+++ src/components/custom/custom-about-community/custom-about-community.vue
@@ -0,0 +1,32 @@
+<template lang='pug' src='./custom-about-community.pug'></template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      activeSlide: 0
+    }
+  },
+  mounted () {
+    this.slideAutomate()
+  },
+  methods: {
+    slideAutomate () {
+      setInterval(() => {
+        this.activeSlide >= (this.content.slides.length - 1) ? this.activeSlide = 0 : this.activeSlide += 1
+      }, 7500)
+    },
+    slideNext () {
+      this.activeSlide >= (this.content.slides.length - 1) ? this.activeSlide = 0 : this.activeSlide += 1
+    },
+    slidePrev () {
+      this.activeSlide <= 0 ? this.activeSlide = (this.content.slides.length - 1) : this.activeSlide -= 1
+    }
+  }
+}
+</script>
