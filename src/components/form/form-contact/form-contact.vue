498774533b12d0b0e71aef2af579b860a58dacbc
luketevers <luketevers@gmail.com>
2019-04-10 14:05:22 -0700

    creates(form-contact): creates on all pages;

---
+++ src/components/form/form-contact/form-contact.vue
@@ -0,0 +1,57 @@
+<template lang="pug" src="./form-contact.pug"></template>
+
+<script>
+import api from 'api'
+import axios from 'axios'
+
+export default {
+  data: () => {
+    return {
+      fullName: '',
+      guardName: '',
+      clientEmail: '',
+      clientPhone: '',
+      postUrl: api + '/rg-mail/v1/contact',
+      formSubmitted: false,
+      formSuccess: false
+    }
+  },
+  methods: {
+    validate () {
+      this.$validator.validateAll()
+        .then(result => {
+          if (result) {
+            this.onSubmit()
+            console.log('validated')
+          }
+        })
+        .catch((e) => {
+          console.log(e, 'validate')
+        })
+    },
+    onSubmit () {
+      this.formSubmitted = true
+      axios.post(this.postUrl, {
+        fullName: this.fullName,
+        guardName: this.guardName,
+        clientEmail: this.clientEmail,
+        clientPhone: this.clientPhone
+      })
+        .then(res => {
+          this.formSuccess = true
+          setTimeout(() => {
+            this.formSubmitted = false
+            console.log('submitted', this.formSubmitted, this.formSuccess)
+          }, 1500)
+          this.fullName = ''
+          this.guardName = ''
+          this.clientEmail = ''
+          this.clientPhone = ''
+        })
+        .catch(e => {
+          console.log(e, 'submitted')
+        })
+    }
+  }
+}
+</script>
