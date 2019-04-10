e71e7fb5eefa11418dcc0e8787a389e4793e3833
luketevers <luketevers@gmail.com>
2019-04-09 17:15:27 -0700

    updates(new-patients): creates divisions for subpages adds block components;

--- src/pages/new-patients/new-patients.vue
+++ src/pages/new-patients/new-patients.vue
@@ -1,12 +1,19 @@
 <template lang="pug" src="./new-patients.pug"></template>
 
 <script>
+import BlockTabsWheel from 'components/block/block-tabs-wheel/block-tabs-wheel'
+import BlockTextImage from 'components/block/block-text-image/block-text-image'
+
 export default {
   name: 'new-patients',
   computed: {
     props () {
       return this.$store.state.pages['new-patients']
     }
+  },
+  components: {
+    BlockTabsWheel,
+    BlockTextImage
   }
 }
 </script>
