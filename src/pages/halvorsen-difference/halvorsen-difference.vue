7b2eb3968da8798fccace3fd3b506ef1ef710677
luketevers <luketevers@gmail.com>
2019-04-09 17:15:57 -0700

    updates(halvorsen-difference): finishes on desktop;

--- src/pages/halvorsen-difference/halvorsen-difference.vue
+++ src/pages/halvorsen-difference/halvorsen-difference.vue
@@ -1,12 +1,19 @@
 <template lang="pug" src="./halvorsen-difference.pug"></template>
 
 <script>
+import BlockTabsWheel from 'components/block/block-tabs-wheel/block-tabs-wheel'
+import BlockTestimonials from 'components/block/block-testimonials/block-testimonials'
+
 export default {
   name: 'halvorsen-difference',
   computed: {
     props () {
       return this.$store.state.pages['halvorsen-difference']
     }
+  },
+  components: {
+    BlockTabsWheel,
+    BlockTestimonials
   }
 }
 </script>
