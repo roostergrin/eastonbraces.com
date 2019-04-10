c3846546bbf31eb8a100a4792c6b6feedd0557db
luketevers <luketevers@gmail.com>
2019-04-10 14:02:58 -0700

    updates(custom-about-free//custom-new-free): updates component location;

---
+++ src/components/custom/custom-new-free/custom-new-free.vue
@@ -0,0 +1,16 @@
+<template lang='pug' src='./custom-new-free.pug'></template>
+
+<script>
+import FormFree from 'components/form/form-free/form-free'
+
+export default {
+  props: {
+    content: {
+      type: [Object, Array]
+    }
+  },
+  components: {
+    FormFree
+  }
+}
+</script>
