43ebd7aa9225c7ea63a71125cc12237bd1244b20
luketevers <luketevers@gmail.com>
2019-04-11 10:56:59 -0700

    updates(treatments): adds desktop components;

--- src/pages/treatments/treatments.vue
+++ src/pages/treatments/treatments.vue
@@ -1,12 +1,17 @@
 <template lang="pug" src="./treatments.pug"></template>
 
 <script>
+import BlockGradient from 'components/block/block-gradient/block-gradient'
+
 export default {
   name: 'treatments',
   computed: {
     props () {
       return this.$store.state.pages.treatments
     }
+  },
+  components: {
+    BlockGradient
   }
 }
 </script>
