4cb7d8d97fc4387ca3d9d9fd916bd0ccd41d6594
luketevers <luketevers@gmail.com>
2019-04-12 11:56:02 -0700

    creates(custom-treatments-early): creates on desktop;

---
+++ src/components/custom/custom-treatments-early/custom-treatments-early.vue
@@ -0,0 +1,21 @@
+<template lang='pug' src='./custom-treatments-early.pug'></template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      activeAccord: 0
+    }
+  },
+  methods: {
+    setActive (i) {
+      this.activeAccord = i
+    }
+  }
+}
+</script>
