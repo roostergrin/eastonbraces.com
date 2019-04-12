da3be12b259f04329acd919de83ebe16fddf1b5e
luketevers <luketevers@gmail.com>
2019-04-12 11:54:53 -0700

    updates(email.php): adds the php for the forms;

--- dist/functions/email.php
+++ dist/functions/email.php
@@ -6,6 +6,10 @@ function rg_register_routes () {
     'methods' => WP_REST_Server::CREATABLE,
     'callback' => 'rg_serve_route'
   ));
+  register_rest_route('rg-mail/v1', 'free', array(
+    'methods' => WP_REST_Server::CREATABLE,
+    'callback' => 'rg_serve_route_free'
+  ));
 }
 // function for handling post request to new api route
 function rg_serve_route () {
@@ -19,15 +23,61 @@ function rg_serve_route () {
   $subject = 'API Contact Form';
   $headers = array('Content-Type: text/html; charset=UTF-8');
   $message = '<html><body>';
-  $message .= '<p><h4><strong>Form Submission by: </strong></h4>' . $data['firstname'] . '' . $data['lastname'] . '</p>';
+  $message .= '<p><h4><strong>Form Submission for: </strong></h4>' . $data['fullName'] . '</p>';
+  $message .= '<p><h4><strong>Parent/Guardian: </strong></h4>' . $data['guardName'] . '</p>';
   $message .= '<p><h4><strong>Email: </strong></h4>' . $data['email'] . '</p>';
-  $message .= '<p><h4><strong>Message:</strong></h4> ' . $data['message'] . '</p>';
+  $message .= '<p><h4><strong>Phone: </strong></h4>' . $data['phone'] . '</p>';
   $message .= '</body></html>';
 
   $table_name = $wpdb->prefix . 'emails';
 
   $charset_collate = $wpdb->get_charset_collate();
-  
+
+  $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+    `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+    `email` text NOT NULL,
+    `message` text NOT NULL,
+    UNIQUE (`id`)
+  ) $charset_collate;";
+
+  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+  dbDelta( $sql );
+
+  $wpdb->insert($table_name, array(
+    'email' => (string)$data['email'],
+    'message' => (string)$message
+  ));
+
+  $sent_message = wp_mail($to, $subject, $message, $headers);
+
+  if ($sent_message) {
+    echo 'Email has been received!';
+  } else {
+    echo 'Could not send email.';
+  }
+}
+function rg_serve_route_free () {
+  require('wp-load.php');
+
+  global $wpdb;
+
+  $data = json_decode(file_get_contents("php://input"), true);
+  $from = 'info@wordpress.com';
+  $to = 'matt@roostergrin.com';
+  $subject = 'API Contact Form';
+  $headers = array('Content-Type: text/html; charset=UTF-8');
+  $message = '<html><body>';
+  $message .= '<p><h4><strong>Form Submission for: </strong></h4>' . $data['fullName'] . '</p>';
+  $message .= '<p><h4><strong>Parent/Guardian: </strong></h4>' . $data['guardName'] . '</p>';
+  $message .= '<p><h4><strong>Email: </strong></h4>' . $data['email'] . '</p>';
+  $message .= '<p><h4><strong>Phone: </strong></h4>' . $data['phone'] . '</p>';
+  $message .= '</body></html>';
+
+  $table_name = $wpdb->prefix . 'emails';
+
+  $charset_collate = $wpdb->get_charset_collate();
+
   $sql = "CREATE TABLE IF NOT EXISTS $table_name (
     `id` mediumint(9) NOT NULL AUTO_INCREMENT,
     `email` text NOT NULL,
