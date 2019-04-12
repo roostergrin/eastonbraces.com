a382432d2eeb7e03d5cde9dd84fa0b90bf1909c4
luketevers <luketevers@gmail.com>
2019-04-12 11:55:18 -0700

    creates(custom-treatments-braces): creates on desktop;

---
+++ src/components/custom/custom-treatments-braces/custom-treatments-braces.vue
@@ -0,0 +1,21 @@
+<template lang='pug' src='./custom-treatments-braces.pug'></template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      activeTab: 0
+    }
+  },
+  methods: {
+    setActive (i) {
+      this.activeTab = i
+    }
+  }
+}
+</script>
