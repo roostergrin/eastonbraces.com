91dd15d318b52eeb1a34b4b791d8b94dfa99b9cd
luketevers <luketevers@gmail.com>
2019-04-17 14:44:51 -0700

    creates(custom-treatments-color): completes on desktop;

---
+++ src/components/custom/custom-treatments-color/custom-treatments-color.vue
@@ -0,0 +1,30 @@
+<template lang='pug' src='./custom-treatments-color.pug'></template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      activeColor: null,
+      band: []
+    }
+  },
+  mounted () {
+    this.content.colors.forEach((item) => {
+      this.band.push('#000')
+    })
+  },
+  methods: {
+    setActiveColor (color, index) {
+      this.activeColor = color
+    },
+    setBandColor (i) {
+      this.$set(this.band, i, this.activeColor)
+    }
+  }
+}
+</script>
