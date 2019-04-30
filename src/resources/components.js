417871eae9570830585995d16c6e48d558b5e93e
luketevers <luketevers@gmail.com>
2019-04-30 11:27:50 -0700

    updates(loader): adds loader to project;

--- src/resources/components.js
+++ src/resources/components.js
@@ -3,12 +3,14 @@ import BaseBGImg from 'components/base/base-bg-img/base-bg-img'
 import BaseContainer from 'components/base/base-container/base-container'
 import BaseHero from 'components/base/base-hero/base-hero'
 import BaseIcon from 'components/base/base-icon/base-icon'
+import BaseLoader from 'components/base/base-loader/base-loader'
 
 const components = () => {
   Vue.component('BaseBGImg', BaseBGImg)
   Vue.component('BaseContainer', BaseContainer)
   Vue.component('BaseHero', BaseHero)
   Vue.component('BaseIcon', BaseIcon)
+  Vue.component('BaseLoader', BaseLoader)
 }
 
 export default components()
