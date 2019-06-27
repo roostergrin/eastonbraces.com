20da3947958c637e7fc8a5106f98b6da123b387c
luketevers <luketevers@gmail.com>
2019-06-27 15:33:39 -0700

    creates(custom-about-meet): creates on all screens and devices;

---
+++ src/components/custom/custom-about-meet/custom-about-meet.vue
@@ -0,0 +1,21 @@
+<template lang='pug' src='./custom-about-meet.pug'></template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      activeImage: 0
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      this.activeImage === this.content.slider.length - 1 ? this.activeImage = 0 : this.activeImage++
+    }, 4000)
+  }
+}
+</script>
