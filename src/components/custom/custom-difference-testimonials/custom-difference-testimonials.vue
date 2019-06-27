db2ca3a2fa6012bafc28cad88930cf05fcc92a81
luketevers <luketevers@gmail.com>
2019-06-27 15:35:58 -0700

    creates(custom-difference-testimonials): creates on all screens and devices;

---
+++ src/components/custom/custom-difference-testimonials/custom-difference-testimonials.vue
@@ -0,0 +1,37 @@
+<template lang='pug' src='./custom-difference-testimonials.pug'></template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      showing: 0
+    }
+  },
+  computed: {
+    slideList () {
+      return this.content.slides
+    }
+  },
+  mounted () {
+    this.slideChange()
+  },
+  methods: {
+    slideChange () {
+      setInterval(() => {
+        this.showing === this.slideList.length - 1 ? this.showing = 0 : this.showing++
+      }, 7500)
+    },
+    slideNext () {
+      this.showing >= (this.slideList.length - 1) ? this.showing = 0 : this.showing += 1
+    },
+    slidePrev () {
+      this.showing <= 0 ? this.showing = (this.slideList.length - 1) : this.showing -= 1
+    }
+  }
+}
+</script>
