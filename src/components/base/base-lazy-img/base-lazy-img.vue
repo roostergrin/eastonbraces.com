2ce234df6cfba72803bfeb09120a095889374e2f
luketevers <luketevers@gmail.com>
2019-06-27 15:28:55 -0700

    creates(base-lazy-img): creates new lazy image loader;

---
+++ src/components/base/base-lazy-img/base-lazy-img.vue
@@ -0,0 +1,50 @@
+<template lang='pug' src='./base-lazy-img.pug'></template>
+
+<script>
+import gif from './animatedimage.gif'
+
+export default {
+  props: {
+    src: {
+      type: String
+    },
+    bgColor: {
+      type: String,
+      default: '#f8f8f8'
+    },
+    alt: {
+      type: String,
+      default: 'I am a decorative image.'
+    }
+  },
+  data () {
+    return {
+      currentImg: null,
+      animatedGif: gif,
+      loading: true,
+      loaded: false,
+      intersectionOptions: {
+        root: null,
+        rootMargin: '500px 0px 0px 0px',
+        threshold: [0]
+      }
+    }
+  },
+  mounted () {
+    this.loaded = true
+  },
+  methods: {
+    onWaypoint ({ going, direction }) {
+      if (going === 'in') {
+        let downloadingImage = new Image()
+        this.currentImg = this.src
+        downloadingImage.onload = () => {
+          this.loading = false
+          console.log('loaded')
+        }
+        downloadingImage.src = this.src
+      }
+    }
+  }
+}
+</script>
