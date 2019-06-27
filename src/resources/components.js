6354c457c58cad29a3da1652b58e4cb0f93bf0e1
luketevers <luketevers@gmail.com>
2019-06-27 15:41:09 -0700

    updates(components.js): adds new lazy loaders;

--- src/resources/components.js
+++ src/resources/components.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
-import BaseBGImg from 'components/base/base-bg-img/base-bg-img'
+import BaseLazyImg from 'components/base/base-lazy-img/base-lazy-img'
+import BaseLazyBg from 'components/base/base-lazy-bg/base-lazy-bg'
 import BaseContainer from 'components/base/base-container/base-container'
 import BaseHero from 'components/base/base-hero/base-hero'
 import BaseIcon from 'components/base/base-icon/base-icon'
 import BaseLoader from 'components/base/base-loader/base-loader'
 
 const components = () => {
-  Vue.component('BaseBGImg', BaseBGImg)
+  Vue.component('BaseLazyImg', BaseLazyImg)
+  Vue.component('BaseLazyBg', BaseLazyBg)
   Vue.component('BaseContainer', BaseContainer)
   Vue.component('BaseHero', BaseHero)
   Vue.component('BaseIcon', BaseIcon)
