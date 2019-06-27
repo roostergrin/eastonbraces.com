0a0a339f08ac2c0fb47b96270ec7e63e6f93ee25
luketevers <luketevers@gmail.com>
2019-06-27 15:31:18 -0700

    updates(block-text-image): adds revolving image functionality;

--- src/components/block/block-text-image/block-text-image.vue
+++ src/components/block/block-text-image/block-text-image.vue
@@ -6,6 +6,16 @@ export default {
     content: {
       type: [Object, Array]
     }
+  },
+  data () {
+    return {
+      activeImage: 0
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      this.activeImage === this.content.slider.length - 1 ? this.activeImage = 0 : this.activeImage++
+    }, 4000)
   }
 }
 </script>
