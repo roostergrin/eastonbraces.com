a5f6ed4f7a8cc95104a71e69443363fd1dc1fecb
GreenWebMedia <37212563+GreenWebMedia@users.noreply.github.com>
2022-10-07 17:51:35 +0530

    Invi Gallery Added
    Invi Gallery Added

--- src/components/block/block-gradient/block-gradient.vue
+++ src/components/block/block-gradient/block-gradient.vue
@@ -6,6 +6,31 @@ export default {
     content: {
       type: [Object, Array]
     }
+  },
+  data () {
+    return {
+      swiperOptioninvibeforeafter: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        loop: true,
+        spaceBetween: 10,
+        speed: 1000,
+        observer: true,
+        observeParents: true,
+        autoplay: {
+          delay: 50000,
+          disableOnInteraction: false
+        },
+        pagination: {
+          el: '.invislidernew .swiper-pagination',
+          clickable: true
+        },
+        navigation: {
+          nextEl: '.invislidernew .swiper-button-next',
+          prevEl: '.invislidernew .swiper-button-prev'
+        }
+      }
+    }
   }
 }
 </script>
