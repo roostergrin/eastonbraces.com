04c704ba5b2c8fdc3270adf18f4a74628b14b3ba
GreenWebMedia <37212563+GreenWebMedia@users.noreply.github.com>
2024-09-23 14:34:28 +0530

    SEO Points Added
    SEO Points Added

--- dist/index.php
+++ dist/index.php
@@ -4,18 +4,45 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" id="sb-font-awesome-css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css" media="all">
-	<!-- Google tag (gtag.js) -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=G-0JRC63C5R8"></script>
-		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
+  <link rel="preload" as="image" href="https://eastonbraces.com/wp-content/uploads/hero2.jpg" />
+  
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-PBMTLD42');</script>
+  <!-- End Google Tag Manager -->
+  
+  <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0JRC63C5R8"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
-		  gtag('config', 'G-0JRC63C5R8');
-		</script>
+      gtag('config', 'G-0JRC63C5R8');
+    </script>
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-BGRNDSPV0B"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-BGRNDSPV0B');
+  </script>
+  
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+  
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBMTLD42"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->  
+  
   <div id='app' role='application'></div>
   <?php wp_footer(); ?>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyClno4ZMvwmys-6PSHJabuspzz1ZdKJWkA" async defer></script>
@@ -45,5 +72,29 @@
     (d.body || d.head).appendChild(ns);})(document);   
 
   </script>
+  <script>
+    let links = document.querySelectorAll('a');
+    for (let i = 0; i < links.length; i++) {
+        if (!links[i].hasAttribute('title')) {
+            links[i].setAttribute("title", "Link");
+        }
+    }
+</script>
+<script>
+    let iframes = document.querySelectorAll('iframe');
+    for (let i = 0; i < iframes.length; i++) {
+        if (!iframes[i].hasAttribute('title')) {
+            iframes[i].setAttribute("title", "embedded");
+        }
+    }
+</script>
+<script>
+    let images = document.querySelectorAll('img');
+    images.forEach(image => {
+        if (!image.hasAttribute('alt')) {
+            image.setAttribute('alt', 'Image');
+        }
+    });
+</script>
 </body>
 </html>
